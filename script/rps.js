76a8530ecf776226d0f7887b69af81c291ca20c3
ddcroft73 <gen.disarray73@outlook.com>
2022-06-10 21:15:47 -0400

    got rid of Puter all over.

--- script/rps.js
+++ script/rps.js
@@ -9,10 +9,9 @@ const profiles = {
 let gameRounds = 0;
 let gameCnt = 0;
 let humanScore = 0;
-let puterScore = 0;
+let computerScore = 0;
 let humanScore_running = 0;
-let puterScore_running = 0;
-
+let computerScore_running = 0;
 let roundWinner = null;
 
 const rock = document.querySelector('#rock');
@@ -21,7 +20,7 @@ const scissors = document.querySelector('#scissors');
 const play =  document.querySelector('#play');
 //const clear = document.querySelector('#clear');
 const human_score = document.querySelector('#human-score');
-const puter_score = document.querySelector('#puter-score');
+const computer_score = document.querySelector('#puter-score');
 
 
 rock.addEventListener('click', (e) => setUserChoice(e.target.id));
@@ -42,19 +41,19 @@ let getFileNameOnly = (path) => {
 }
 
 const setUserChoice = (choice) => {
-    clearPuterChoice()
+    clearComputerChoice()
     choice = 'image/' + choice + '.png';
     const img = document.querySelector("#human-choice");
     img.src = choice;
 }
 
-const clearPuterChoice = () => {
+const clearComputerChoice = () => {
     choice = 'image/puter.png';
     const img = document.querySelector("#puter-choice");
     img.src = choice;
 }
 
-let getPuterChoice = (choice) => {         
+let getComputerChoice = (choice) => {         
     let selection = '';
     if (choice === 1) {
         selection = 'image/rock.png';            
@@ -70,18 +69,19 @@ let getPuterChoice = (choice) => {
     return selection;
 }
 
-//Play the round
+//Play a round
 const throwHands = () => {           
     gameCnt++;  
+
     let userChoice = queryUserChoice();    
     if (userChoice === 'image/user.png') {
         statusReport("Please <span>select a weapon</span>.");
         return;
     }
-    // didnt overthink the AI in this. 
+    // didnt overthink the AI on this. 
     let selection = Math.floor(Math.random() * 3) + 1;                
-    let puterChoice = getPuterChoice(selection);
-    let winner = getWinner(userChoice, puterChoice);  
+    let computerChoice = getComputerChoice(selection);
+    let winner = getWinner(userChoice, computerChoice);      
     let trueGameCount = updateScore(winner); 
     updateStatus(winner, trueGameCount); 
 }
@@ -90,7 +90,7 @@ const newGame = () => {
 
     gameCnt = 0;
     humanScore = 0;
-    puterScore = 0;
+    computerScore = 0;
 
     // let the user play again.
     document.getElementById("play").disabled = false; 
@@ -101,21 +101,21 @@ const newGame = () => {
 
     const imgPuter = document.querySelector("#puter-choice");        
     imgPuter.src = 'image/puter.png';
-    puter_score.textContent = '0';
+    computer_score.textContent = '0';
 
     statusReport('<span>NEW GAME</span>?');
 }
 
-//defined an object with any profile where the user will win. Compare that to the current
+// defined an object with any profile where the user will win. Compare that to the current
 // profile of user vs puter. If the user didn't win and there was not a tie. Computer won.
-const getWinner = (user, puter) => {
+const getWinner = (user, computer) => {
     let curWinner = 'puter';    
-    const profile = getFileNameOnly(user) + ' ' + getFileNameOnly(puter);
+    const profile = getFileNameOnly(user) + ' ' + getFileNameOnly(computer);
     
     for (var i = 0, l = profiles.userWins.length; i < l; i++) {
         let win = profiles.userWins[i];
 
-        if (user === puter) {           
+        if (user === computer) {           
             curWinner = 'tie';
             break;
         } 
@@ -124,7 +124,6 @@ const getWinner = (user, puter) => {
             break;                  
         }  
     }
-    //updateStatus(winner);  
     return curWinner;
 }
 
@@ -136,7 +135,8 @@ const updateStatus = (winner, actualGameCount) => {
 
     //  if the game is in the last round display a last round message. If the next round is a tie
     //  continue to display the last round message.     
-    if (actualGameCount === 4) { // actualGameCount gives a true real time count vs gameCnt which can lag.
+
+    if (actualGameCount === 4) { 
         statusReport('<span>Last Point</span>!!: [<span>Who will it be?!</span>] ' );    
     } else if(actualGameCount === 5 && actualGameCount === 'Tie Game') {   
         statusReport('<span>Last Point...</span>: [<span>but it was a tie!</span>] ');     
@@ -151,46 +151,47 @@ const updateScore = (winner) => {
         human_score.textContent = humanScore;
     }    
     if (winner === 'puter') {
-        puterScore++;
-        puter_score.textContent = puterScore;
+        computerScore++;
+        computer_score.textContent = computerScore;
     }    
     if (winner === 'tie') {
         gameCnt--;       
     } 
-
-    //on game 5, report the results. 
+    // game over
     if (gameCnt === 5) {
         reportResults();    
         // give the user a second to see the last results before resetting.
         document.getElementById("play").disabled = true; 
         setTimeout(newGame, 1500);          
-    }       
-    
-    return humanScore + puterScore;
+    }           
+    // actualGameCount gives a true real time count vs gameCnt which can lag.
+    // This still is not 100% I probably need to rethink how i track the games and where.
+    // because I really should not need to do this.
+    return humanScore + computerScore;
 }
 
 // Final report after the race to 5.
 const reportResults = () => {
     gameRounds++;
-    if (humanScore > puterScore) {
+    if (humanScore > computerScore) {
         roundWinner = "Player";
         humanScore_running++;
     }
     else {
         roundWinner = 'Computer';
-        puterScore_running++;
+        computerScore_running++;
     }    
-    let textContent = '<span>Round ' + gameRounds + '</span>:  [<span>' + humanScore + '</span>] [<span>' + puterScore + '</span>] '+ roundWinner + ' wins.';
+    let textContent = '<span>Round ' + gameRounds + '</span>:  [<span>' + humanScore + '</span>] [<span>' + computerScore + '</span>] '+ roundWinner + ' wins.';
     
     appendResults(textContent);       
-    showMatchResults();
-    
+    showMatchResults();    
 }
 
 const statusReport = (message) => {
     const resultContainer = document.querySelector('.status');
     resultContainer.innerHTML = message;
 }
+
 const appendResults = (message) => {
     // add a div in the "results" section to display the results.
     const resultContainer = document.querySelector('.results');
@@ -203,36 +204,32 @@ const appendResults = (message) => {
 }
 
 /**
- * if gameRounds is divisible by 3 see what the scores are and make a report. 
+ * if gameRounds is divisible by 3 see what the scores are and make a report.
+ * if user or puter is ahead by more than 1 game, add an "s" the the end of game. 
  */
 const showMatchResults = () => {    
     let message = '';
     let modifier = '';
 
-
      if (gameRounds % 3 === 0)  {
-        if (puterScore_running > humanScore_running) {
+        if (computerScore_running > humanScore_running) {
 
-            if (puterScore_running > (humanScore_running+1)) {
+            if (computerScore_running > (humanScore_running+1)) {
                modifier = 's';
             }
-             message = '<span>The computer has you by</span> ' + ((puterScore_running - humanScore_running)) + ' <span>game' +modifier +'</span>.';   
+             message = '<span>The computer has you by</span> ' + ((computerScore_running - humanScore_running)) + ' <span>game' +modifier +'</span>.';   
              statusReport(message);
-        } else if (humanScore_running > puterScore_running){
-            if (humanScore_running > (puterScore_running+1)) {
+        } else if (humanScore_running > computerScore_running){
+            if (humanScore_running > (computerScore_running+1)) {
                 modifier = 's';
              } 
-            message ='<span>Your\'e in the lead! </span> ' + ((humanScore_running - puterScore_running)) + ' <span>game' +modifier +'</span>.';
+            message ='<span>Your\'e in the lead! </span> ' + ((humanScore_running - computerScore_running)) + ' <span>game' +modifier +'</span>.';
             statusReport(message);
-        }else if (humanScore_running === puterScore_running){
-            message = '<span>The Match is tied up </span> [<span>' + humanScore_running+ '</span>] <span>to</span> [<span>' + puterScore_running +'</span>]!';
+        }else if (humanScore_running === computerScore_running){
+            message = '<span>The Match is tied up </span> [<span>' + humanScore_running+ '</span>] <span>to</span> [<span>' + computerScore_running +'</span>]!';
             statusReport(message);
         }
      }    
 
      appendResults(message);
-}
-
-let trueGameCount = () => {
-
 }
